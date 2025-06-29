16df98da3965f6ab288d76c39a243bb0d506f2b6
André Victor Gonçalves Nascimento <andrevictor20011@gmail.com>
2025-06-29 19:38:12 -0300

    Added Observer Pattern Base
    Added the abstract class Subject and the interface IObserver.

--- Assets/Scripts/Utils/Subject.cs
+++ Assets/Scripts/Utils/Subject.cs
@@ -1,7 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Subject : MonoBehaviour
 {
+    private List<IObserver> _subscribers = new List<IObserver>();
+
+    public void Subscribe(IObserver observer)
+    {
+        _subscribers.Add(observer);
+    }
+
+    public void Unsubscribe(IObserver observer)
+    {
+        _subscribers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        _subscribers.ForEach((_observer) => {_observer.OnNotify();});
+    }
 }
