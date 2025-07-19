7bd374de5ac4de0754599bfa67c4361258e4a56f
Tiago_hMr <123904927+tiagomello23@users.noreply.github.com>
2025-07-19 18:40:14 -0300

    Floor tags added

--- Assets/Scripts/Elevator/ElevatorManager.cs
+++ Assets/Scripts/Elevator/ElevatorManager.cs
@@ -29,6 +29,7 @@ public class ElevatorManager : Subject
         HandleMovement();
     }
 
+    /*
     private void HandleMovement()
     {
         if(this.gameObject.transform.position.y == _initialYPosition)
@@ -52,8 +53,31 @@ public class ElevatorManager : Subject
 
             StartCoroutine(ChangeFloor(_currentFloor));
         }
+    }*/
+    private void HandleMovement() {
+        float currentY = this.gameObject.transform.position.y;
+        float tolerance = 0.01f;
+
+        for (int i = 0; i < 20; i++)
+        {
+            float expectedY = _initialYPosition - (2.22f * i);
+            if (Mathf.Abs(currentY - expectedY) < tolerance)
+            {
+                _currentFloor = i + 1;
+
+                if (!_alreadyPassed.Contains(_currentFloor))
+                    _alreadyPassed.Add(_currentFloor);
+
+                // Uncomment to trigger events if needed
+                // Notify((EventsEnum)_currentFloor);
+
+                StartCoroutine(ChangeFloor(_currentFloor));
+                break;
+            }
+        }
     }
 
+    /*
     public IEnumerator ChangeFloor(int currentFloor)
     {
         yield return new WaitForSeconds(3f);
@@ -82,6 +106,52 @@ public class ElevatorManager : Subject
             _currentTween = transform.DOLocalMoveY(_scaleY - 1.24f, 2f);
             Notify(EventsEnum.SECOND_FLOOR);
             _wentUp = true;
+        }
+    }*/
+    public IEnumerator ChangeFloor(int currentFloor){
+        yield return new WaitForSeconds(3f);
+
+        _currentTween?.Kill();
+
+        // Middle state (used as "buffer")
+        float middleY = _scaleY - 1.24f;
+
+        int playerFloor = PlayerController.Instance.GetCurrentFloor();
+
+        // Going up logic
+        bool goingUp = (playerFloor < currentFloor && _alreadyPassed.Contains(currentFloor + 1)) || _wentUp;
+
+        if (currentFloor == 1)
+        {
+            _currentTween = transform.DOLocalMoveY(middleY, 2f);
+            Notify(EventsEnum.SECOND_FLOOR);
+            _wentUp = false;
+        }
+        else if (currentFloor == 20)
+        {
+            // Max floor: just go back down
+            _currentTween = transform.DOLocalMoveY(_initialYPosition - (2.22f * 18), 2f); // floor 19's position
+            Notify((EventsEnum)19); // Assuming you have enums like FIRST_FLOOR = 1, etc.
+            _wentUp = false;
+        }
+        else
+        {
+            if (goingUp)
+            {
+                // Go up
+                float newY = _initialYPosition - (2.22f * (currentFloor - 2)); // Go to floor above
+                _currentTween = transform.DOLocalMoveY(newY, 2f);
+                Notify((EventsEnum)(currentFloor - 1));
+                _wentUp = true;
+            }
+            else
+            {
+                // Go down
+                float newY = _initialYPosition - (2.22f * currentFloor); // Go to floor below
+                _currentTween = transform.DOLocalMoveY(newY, 2f);
+                Notify((EventsEnum)(currentFloor + 1));
+                _wentUp = false;
+            }
         }
     }
 }
